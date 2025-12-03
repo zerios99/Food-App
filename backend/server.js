12eae3163e5d06d8cf6e9502b78c765a588103e7
zain yasser <zainyasser31@gmail.com>
2025-12-03 15:51:46 +0200

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
