6610eba0c3f94cbcde59410007a47c0a0d1532d5
zain yasser <zainyasser31@gmail.com>
2025-12-08 00:24:17 +0200

    add favorites endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,44 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
 
 const app = express();
-
 const port = ENV.PORT || 8001;
+
+app.use(express.json());
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true });
 });
 
-app.post("/api/favorite", async (req, res) => {
+app.post("/api/favorites", async (req, res) => {
   try {
     const { userId, recipeId, title, image, cookTime, servings } = req.body;
-  } catch (error) {}
+
+    if (!userId || !recipeId || !title) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing required fields" });
+    }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json({ success: true, data: newFavorite });
+  } catch (error) {
+    console.error("Error adding favorite:", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(port, () => {
