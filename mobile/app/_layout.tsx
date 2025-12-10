37bc01da7de688a1593cfc5ba870e714a91abff8
zain yasser <zainyasser31@gmail.com>
2025-12-10 18:47:46 +0200

    some updates

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar style="dark" />
+    </SafeAreaView>
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Link href="/about">about</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "blue",
+    fontSize: 30,
+  },
+});
