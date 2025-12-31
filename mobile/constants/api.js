77e3b358a797bc8c24451f869e6928c164d10bc4
zerios <zainyasser31@gmail.com>
2025-12-31 16:50:05 +0200

    add some fixes for the api

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,3 +1,3 @@
 // if you're using your physical phone, change this to the deployed url
 // we have explained this in the course :-)
-export const API_URL = "http://localhost:5001/api";
+export const API_URL = "https://food-app-indol-nine.vercel.app/api";
